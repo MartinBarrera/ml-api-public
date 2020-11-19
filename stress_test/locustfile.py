ae494bf1f4fdf60e4bdabd31499e363f14b9537d
tincho <martin.barrera@outlook.com>
2020-11-19 20:58:36 -0300

    stress test

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -1,15 +1,16 @@
 import time
 from locust import HttpUser, task
 
+
 class QuickstartUser(HttpUser):
 
     @task(1)
     def index(self):
-        raise NotInplementedError
+        self.client.get('/')
 
     @task(3)
     def predict(self):
-        raise NotInplementedError
+        self.client.post('/predict', params={'text': 'un texto cualquiera'})
 
     def on_start(self):
         pass
